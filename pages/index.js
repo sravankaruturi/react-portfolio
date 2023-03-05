175570e61c00c2233df1da5476aedec26b4ce95b
Sravan Karuturi <sravankumarkaruturi@gmail.com>
2023-03-04 23:29:24 -0800

    Cleanse

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import React from "react"
-
 export default function Home() {
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
     </h1>
   )
 }
